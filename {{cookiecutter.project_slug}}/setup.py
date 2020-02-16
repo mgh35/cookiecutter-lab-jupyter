b0777a27117319098e500fd9608ed219f4b02bcf
mark <>
2020-02-16 09:44:45 -0400

    Basic template

---
+++ {{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="{{cookiecutter.project_name}}",
+    version="0.0.1",
+    author="{{cookiecutter.full_name}}",
+    description="{{cookiecutter.project_description}}",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ]
+)
